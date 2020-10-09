3e40062ec225d6965f16fd3b81ca7aaaf779fae5
erinszabo <55733370+erinszabo@users.noreply.github.com>
2020-10-08 17:23:47 -0700

    Initial commit

---
+++ pdf_pg60.py
@@ -0,0 +1,11 @@
+"""
+
+Devise an experiment to verify that the list index operator is 𝑂(1).
+2. Devise an experiment to verify that get item and set item are 𝑂(1) for dictionaries.
+3. Devise an experiment that compares the performance of the del operator on lists and
+dictionaries.
+4. Given a list of numbers in random order write a linear time algorithm to find the 𝑘th
+smallest number in the list. Explain why your algorithm is linear.
+5. Can you improve the algorithm from the previous problem to be 𝑂(𝑛 log(𝑛))?
+
+"""
